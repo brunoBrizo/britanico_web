899d874d1b17a683da9a8e84b6afd2f1a4c151b4
brunoBrizo <bbrizolara7@gmail.com>
2021-05-24 11:57:09 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:britanico_web/constants.dart';
+import 'package:britanico_web/sections/mainSection.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instituto Británico',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: kPrimaryColor,
+        accentColor: kPrimaryColor,
+        // fontFamily: "Montserrat",
+        highlightColor: kPrimaryColor,
+      ),
+      home: MainPage(),
+    );
+  }
+}
